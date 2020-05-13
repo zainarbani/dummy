db659b465c789636a453eefe1599dfb7d91e7c02
Zain Arbani <zaintsyariev@gmail.com>
2020-05-13 22:37:23 +0700

    Update mirom.py

--- mirom.py
+++ mirom.py
@@ -1,56 +1,59 @@
-#!/usr/bin/env python3.7
-
-import requests
-import json
 import os
+
 from git import Repo
+from json import dump
+from requests import get
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-def git_push():
-    git_user = ''#gh username
-    git_pass = ''#gh userpass
-    repo_name = 'dummy'#gh repo
-    remote_url = f'https://{git_user}:{git_pass}@github.com/{git_user}/{repo_name}.git'
-    repo = Repo('.git')
 
-    if 'modified' in repo.git.status(['mirom.json']):
-        print('New update found !')
-        repo.index.add(['mirom.json'])
-        repo.index.commit('mirom: ' + datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
-        repo.git.push(remote_url, 'master')
+def git_push():
+    git_user = ""  # github username
+    git_pass = ""  # github userpass
+    repo_name = "dummy"  # github repo
+    remote_url = f"https://{git_user}:{git_pass}@github.com/{git_user}/{repo_name}.git"
+    repo = Repo(".git")
+
+    if "modified" in repo.git.status(["mirom.json"]):
+        print("New update found !")
+        repo.index.add(["mirom.json"])
+        repo.index.commit("mirom: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+        repo.git.push(remote_url, "master")
     else:
-        print('No update found !')
+        print("No update found !")
+
 
 def miui(pid):
-    url = f'http://c.mi.com/oc/rom/getdevicelist?phone_id={pid}'
-    res = requests.get(url).json()
+    url = f"http://c.mi.com/oc/rom/getdevicelist"
+    res = get(url, params={"phone_id": pid}).json()
+    for x in res["data"]["device_data"]["device_list"]:
+        url = res["data"]["device_data"]["device_list"][f"{x}"]["stable_rom"]["rom_url"]
+        data.append({"phone": x, "url": {"global": url, "ru": url, "id": url}})
 
-    for device in res['data']['device_data']['device_list']:
-        rom = res['data']['device_data']['device_list'][f'{device}']['stable_rom']['rom_url']
-        with open('mirom.json', 'a+') as f:
-            json.dump({'device': device, 'url': rom}, f, indent=1)
-            f.write(',\n')
 
 def main():
     try:
-        os.remove('mirom.json')
+        os.remove("mirom.json")
     except OSError:
         pass
-    
-    print("Fetching update, started at:", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
-    url = 'https://c.mi.com/oc/rom/getphonelist'
-    res = requests.get(url).json()
 
-    for device in res['data']['phone_data']['phone_list']:
-        pid = device['id']
+    print("Fetching update, started at:", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+    url = "https://c.mi.com/oc/rom/getphonelist"
+    res = get(url).json()
+    for x in res["data"]["phone_data"]["phone_list"]:
+        pid = x["id"]
         miui(pid)
+
+    with open("mirom.json", "w") as f:
+        dump(data, f, indent=2)
 
     git_push()
     print("Fetching update done\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    data = []
     main()
     scheduler = BlockingScheduler()
-    scheduler.add_job(main, 'interval', minutes=30)
+    scheduler.add_job(main, "interval", minutes=30)
     scheduler.start()
